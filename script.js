6313691993f7d15eac74161504cecb9be775eac6
prathamesh-ab <prathamesh.bhanse18@gmail.com>
2023-09-12 10:39:35 +0530

    Created HTML layout

---
+++ script.js
@@ -0,0 +1,15 @@
+const inputSlider = document.querySelector("[data-lengthSlider]");
+const lengthDisplay = document.querySelector("[data-lengthNumber]");
+
+const passwordDisplay = document.querySelector("[display-passWordDisplay]");
+const copyBtn = document.querySelector("[dataCopy]");
+const copyMsg = document.querySelector("[display-copyMsg]");
+const upperCaseCheck = document.querySelector("#uppercase");
+const lowerCaseCheck = document.querySelector("#lowercase");
+const numbersCaseCheck = document.querySelector("#numbers");
+const symbolsCaseCheck = document.querySelector("#symbols");
+const indicator = document.querySelector("[data-indicator]");
+const generateBtn = document.querySelector(".generateButton");
+const allCheckBox = document.querySelector("input[type=checkbox]"); 
+
+console.log(inputSlider.value);
